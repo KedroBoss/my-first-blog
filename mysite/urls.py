2f86c82985274cccb6f2def94d808f9baa4ad1e0
KedroBoss <dimmak90@gmail.com>
2017-01-01 21:14:49 +0300

    First Commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,9 +28,3 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^debug/', include(debug_toolbar.urls)),
-        ]
